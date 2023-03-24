7a9faf236e65e561d1fd0da9313dce90064f4c6e
Helton-Pereira <helton.bfsm@gmail.com>
2023-03-24 19:33:25 -0300

    Teste para counter.py

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,5 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
-def test_counter():
-    pass
+def test_counter_should_return_the_correct_number_of_word_searched():
+    assert count_ocurrences('data/jobs.csv', 'Python') == 1639
