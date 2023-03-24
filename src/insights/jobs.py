7f6e922088b6221cac41d8ea013d0d2732f4a868
Helton-Pereira <helton.bfsm@gmail.com>
2023-03-24 15:47:32 -0300

    req 2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -17,20 +17,15 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
+    list = []
+    list_of_jobs = read(path)
 
-    Must call `read`
+    for job in list_of_jobs:
+        if job['job_type'] not in list:
+            list.append(job['job_type'])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
+    return list
 
-    Returns
-    -------
-    list
-        List of unique job types
-    """
     raise NotImplementedError
 
 
