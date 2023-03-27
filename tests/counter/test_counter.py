2df0194068e96d79b5562f9f785fbec040886609
Helton-Pereira <helton.bfsm@gmail.com>
2023-03-27 15:16:25 -0300

    Refatora teste para aprovação do req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
 from src.pre_built.counter import count_ocurrences
 
 
-def test_counter_should_return_the_correct_number_of_word_searched():
-    assert count_ocurrences('data/jobs.csv', 'Python') == 1639
+def test_counter():
+    assert count_ocurrences('data/jobs.csv', 'xablau') == 0
+    assert count_ocurrences('data/jobs.csv', 'Javascript') == 122
